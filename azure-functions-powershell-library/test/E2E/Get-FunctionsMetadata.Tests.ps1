6ddb5fb1a6c5c01f6ecd23be2ea2664335636e22
Anthony Staples <t-anstaples@microsoft.com>
2022-08-01 09:49:55 -0700

    Very simple first tests

---
+++ azure-functions-powershell-library/test/E2E/Get-FunctionsMetadata.Tests.ps1
@@ -0,0 +1,79 @@
+﻿using module AzureFunctions.PowerShell.SDK
+
+
+Describe 'SinglePs1FunctionApp' {
+        BeforeAll {
+        $metadata = Get-FunctionsMetadata ((Get-Location).Path + "\apps\single-ps1app")
+        $metadataObject = $metadata | ConvertFrom-Json
+
+        Import-Module ".\helpers.psm1" -force
+    }
+    It 'Should return 1 function' {
+        $metadataObject.Count | Should -Be 1
+    }
+
+    It 'Functions name should be correct' {
+        Get-FunctionNames $metadataObject | Should -Contain "TestTrigger"
+    }
+
+    It 'Function Bindings should be correct' {
+        Get-BindingCount $metadataObject 'TestTrigger' | Should -Be 2
+
+        Get-Binding $metadataObject 'TestTrigger' 'httpTrigger' | Should -Not -Be $null
+        Get-Binding $metadataObject 'TestTrigger' 'http' | Should -Not -Be $null
+    }
+}
+
+Describe 'SinglePsm1FunctionApp' {
+        BeforeAll {
+        $metadata = Get-FunctionsMetadata ((Get-Location).Path + "\apps\single-psm1app")
+        $metadataObject = $metadata | ConvertFrom-Json
+
+        Import-Module ".\helpers.psm1" -force
+    }
+    It 'Should return 1 function' {
+        $metadataObject.Count | Should -Be 1
+    }
+
+    It 'Functions name should be correct' {
+        Get-FunctionNames $metadataObject | Should -Contain "TestTrigger"
+    }
+
+    It 'Function Bindings should be correct' {
+        Get-BindingCount $metadataObject 'TestTrigger' | Should -Be 2
+
+        Get-Binding $metadataObject 'TestTrigger' 'httpTrigger' | Should -Not -Be $null
+        Get-Binding $metadataObject 'TestTrigger' 'http' | Should -Not -Be $null
+    }
+}
+
+Describe 'Simple-DurableApp' {
+    BeforeAll {
+        $metadata = Get-FunctionsMetadata ((Get-Location).Path + "\apps\simple-durable")
+        $metadataObject = $metadata | ConvertFrom-Json
+
+        Import-Module ".\helpers.psm1" -force
+    }
+    It 'Should return 3 functions' {
+        $metadataObject.Count | Should -Be 3
+    }
+
+    It 'Functions names should be correct' {
+        Get-FunctionNames $metadataObject | Should -Contain "Hello1"
+        Get-FunctionNames $metadataObject {$_.Name} | Should -Contain "DurableFunctionsHttpStart1"
+        Get-FunctionNames $metadataObject {$_.Name} | Should -Contain "DurableFunctionsOrchestrator1"
+    }
+
+    It 'Function Bindings should be correct' {
+        Get-BindingCount $metadataObject 'Hello1' | Should -Be 1
+        Get-BindingCount $metadataObject 'DurableFunctionsHttpStart1' | Should -Be 3
+        Get-BindingCount $metadataObject 'DurableFunctionsOrchestrator1' | Should -Be 1
+
+        Get-Binding $metadataObject 'Hello1' 'activityTrigger' | Should -Not -Be $null
+        Get-Binding $metadataObject 'DurableFunctionsHttpStart1' 'httpTrigger' | Should -Not -Be $null
+        Get-Binding $metadataObject 'DurableFunctionsHttpStart1' 'http' | Should -Not -Be $null
+        Get-Binding $metadataObject 'DurableFunctionsHttpStart1' 'durableClient' | Should -Not -Be $null
+
+        Get-Binding $metadataObject 'DurableFunctionsOrchestrator1' 'orchestrationTrigger' | Should -Not -Be $null
+    }
+}
---
+++ azure-functions-powershell-library/test/E2E/helpers.psm1
@@ -0,0 +1,28 @@
+﻿
+function Get-FunctionNames {
+    param(
+        $functionObject
+    ) 
+    return $functionObject | ForEach-Object {$_.Name}
+}
+
+function Get-BindingCount {
+    param( 
+        $functionObject,
+        [string]$functionName
+    )
+    return ($functionObject | Where-Object {$_.Name -eq $functionName})[0].Bindings.Count
+}
+
+
+function Get-Binding {
+    param( 
+        $functionObject,
+        [string]$functionName,
+        [string]$bindingType
+    )
+    if ((($functionObject | Where-Object {$_.Name -eq $functionName})[0].Bindings | Where-Object {$_.Type -eq $bindingType}).Count -eq 1) {
+        return (($functionObject | Where-Object {$_.Name -eq $functionName})[0].Bindings | Where-Object {$_.Type -eq $bindingType})[0]
+    }
+    return $null
+}
